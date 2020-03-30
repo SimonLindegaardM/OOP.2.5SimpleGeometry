9f5a2b411ad001ffb9b7dd7546ade83529b62506
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-03-30 13:41:00 +0200

    Initial commit

---
+++ Shape.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleGeometry
+{
+    /// <summary>
+    /// This class is intended to act as
+    /// a base class for geometric shapes
+    /// </summary>
+    public abstract class Shape
+    {
+        private string _shapeName;
+
+        protected Shape(string shapeName)
+        {
+            _shapeName = shapeName;
+        }
+
+        public string ShapeName
+        {
+            get { return _shapeName; }
+        }
+
+        public abstract double Area { get; }
+
+        public static double FindTotalArea(List<Shape> shapes)
+        {
+            // This needs to be changed
+            double Sum=0;
+            foreach (var a in shapes)
+            {
+                Sum += a.Area;
+            }
+            return Sum;
+        }
+    }
+}
